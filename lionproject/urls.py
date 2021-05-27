cf241846bb73c9136ccd8d09f3295034d7616bc0
zerowest04 <youngslee@yonsei.ac.kr>
2021-05-27 18:48:17 +0900

    first commit

--- lionproject/urls.py
+++ lionproject/urls.py
@@ -21,4 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name=""),
     path('<str:id>',detail,name="detail"),
+    path('new/', new, name = "new"),
+    path('create/', create, name = "create"),
+    path('edit/<str:id>', edit, name = "edit"),
+    path('update/<str:id>', update, name = "update"),
+    path('delete/<str:id>', delete, name = "delete"), 
+]
 ]
